516e9ae641aa326d2362e03e9b9b77f1e2f54473
imtiyaz hirani <i.hirani@techskillsit.com>
2020-08-13 12:44:18 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { Customer } from './customer.model';
-import { Observable, Subject, BehaviorSubject } from 'rxjs';
-import { filter } from 'minimatch';
+import { Customer } from './customers/customer.model';
+ 
 
 @Component({
   selector: 'app-root',
@@ -10,48 +9,6 @@ import { filter } from 'minimatch';
 })
 export class AppComponent {
 
-  customer1: Customer = {
-    'name': 'Harry Potter',
-    'age': 20,
-    'isStudent': true,
-    'cgpa': 9.8
-  }
-  customer2: Customer = {
-    'name': 'Roanld Wesley',
-    'age': 20,
-    'isStudent': true,
-    'cgpa': 9.2
-  }
-  customer3: Customer = {
-    'name': 'Hermione Granger',
-    'age': 19,
-    'isStudent': true,
-    'cgpa': 9.9
-  }
-  customer4: Customer = {
-    'name': 'Prof Dumbledor',
-    'age': 70,
-    'isStudent': false,
-    'cgpa': 0.0
-  }
-  customers: Customer[] = [];
-
-  constructor() {
-    this.customers.push(this.customer1);
-    this.customers.push(this.customer2);
-    this.customers.push(this.customer3);
-    this.customers.push(this.customer4);
-  }
-
-  isShow: boolean = true
-  initial: boolean = true
-  showStudent() {
-    this.isShow = true
-    this.initial = false
-  }
-  showNonStudent() {
-    this.isShow = false
-    this.initial = false
-  }
+  
 
 }
